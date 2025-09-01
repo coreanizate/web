5048534133118167ce407fb3d106fc7ec2a1fd65
CCG <coreanizate@gmail.com>
2025-09-01 01:18:41 -0500

    A02

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4173237709"
+    serviceWorkerVersion: "4069263011"
   }
 });
